4ccbda026d1cb2ac040f19818ca975c8d21f31fe
Chen030400 <94821690+Chen030400@users.noreply.github.com>
2021-11-23 15:01:16 +0800

    移植失敗

--- Assets/NumberButtonManager.cs
+++ Assets/NumberButtonManager.cs
@@ -46,56 +46,10 @@ public class NumberButtonManager : MonoBehaviour
     private void Enter()
     {
        
-    //           private void Enter(string guess)
-    //            {
-    //                a = 0;
-    //                b = 0;
-    //                foreach (char i in guess)
-    //                {
-    //                    if (answer.Contains(i))
-    //                    {
-    //                        if (answer.IndexOf(i) == guess.IndexOf(i))
-    //                        {
-    //                            a += 1;
-    //                        }
-    //                        else
-    //                        {
-    //                            b += 1;
-    //                        }
-    //                    }
-    //                }
-    //            }
+   
 
 }
-    //private void NewQuestion(int digit = 4)
-    //{
-    //    string number = "1234567890";
-
-    //    while (true)
-    //    {
-    //        answer = Shuffle(number).Substring(0, digit);
-    //        if (answer[0] != '0')
-    //        {
-    //            break;
-    //        }
-
-    //    }
-    //    string Shuffle(string s)
-    //    {
-    //        char[] s_array = s.ToCharArray();
-    //        Random r = new Random();
-    //        int n = s.Length;
-    //        while (n > 1)
-    //        {
-    //            n--;
-    //            int k = r.Next(n + 1);
-    //            var v = s_array[k];
-    //            s_array[k] = s_array[n];
-    //            s_array[n] = v;
-    //        }
-    //        return new string(s_array);
-    //    }
-    //}
+   
 
         //-----------------------------------------------------------------------------------------------
         private void Remove()
