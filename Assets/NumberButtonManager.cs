48fa0835e7e47e6b913a0080c57cdc8f97824c28
DanielHuang0827 <89367282+DanielHuang0827@users.noreply.github.com>
2021-11-25 21:41:54 +0800

    Update NumberButtonManager.cs
    寫了生成亂數的回圈 怕爆掉 先備份

--- Assets/NumberButtonManager.cs
+++ Assets/NumberButtonManager.cs
@@ -35,7 +35,7 @@ public class NumberButtonManager : MonoBehaviour
     public int B { get => b; set => b = value; }
     public int Times { get => times; set => times = value; }
 
-
+    int fourDigitRandom;
 
     void Start()
     {
@@ -56,9 +56,17 @@ public class NumberButtonManager : MonoBehaviour
 
         Text test = playerAnswer.GetComponent<Text>();
         test.text = number;
+        Random crandom = new Random();
+        
+        for (int i = 0; i < 100; i++)
+        {
+            i = crandom.Next(0, 10);
+            print(i);
+        }
 
-        ;
     }
+
+    
 
     private void Enter()//string guess
     {
